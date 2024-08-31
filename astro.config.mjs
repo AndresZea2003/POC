5f3fe9986d4d02ea5fe3d29f9d16d88b8db42df9
AndresZea2003 <afzea2003@gmail.com>
2024-08-31 08:16:00 -0500

    featL add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel({
+    imageService: true,
+  }),
 });
