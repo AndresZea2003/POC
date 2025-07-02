6828c139dd1f17696d9989e029be4c92ca54eeca
Juan Pabon <juan.pabon@evertecinc.com>
2025-07-02 09:43:08 -0500

    config: change settings to test 3DS behavior

--- src/components/views/GooglePayDemo.vue
+++ src/components/views/GooglePayDemo.vue
@@ -122,8 +122,10 @@ const onGooglePayLoaded = () => {
         {
           type: "CARD",
           parameters: {
-            allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-            allowedCardNetworks: ["AMEX", "DISCOVER", "MASTERCARD", "VISA"],
+            allowedAuthMethods: ['CRYPTOGRAM_3DS'],
+            //allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+            allowedCardNetworks: ['AMEX', 'DISCOVER', 'MASTERCARD', 'VISA'],
+            assuranceDetailsRequired: true
           },
           tokenizationSpecification: {
             type: "PAYMENT_GATEWAY",
