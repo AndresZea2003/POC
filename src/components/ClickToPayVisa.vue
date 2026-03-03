d3f485a7564aa1e80caf3dcf024af1a2794dab0a
Juan Pabon <juan.pabon@evertecinc.com>
2026-03-03 09:48:36 -0500

    config: update src library c2p Visa

--- src/components/ClickToPayVisa.vue
+++ src/components/ClickToPayVisa.vue
@@ -40,8 +40,7 @@ const loadVisaClickToPay = async () => {
   console.log("Cargando script de VISA Click to Pay...");
   const script = document.createElement("script");
   script.src =
-      "https://testup.cybersource.com/uc/v1/assets/0.30.2/SecureAcceptance.js";
-      // "https://apitest.cybersource.com/up/v1/assets/0.22.6/SecureAcceptance.js";
+      "https://up.cybersource.com/uc/v1/assets/0.30.2/SecureAcceptance.js";
   script.async = true;
   script.onload = () => console.log("Script VISA cargado ✅");
   script.onerror = () => console.error("Error al cargar el script de VISA.");
